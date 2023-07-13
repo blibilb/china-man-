74d0980ac3fa8c68facba14165911b65e2df2ae1
bliblib <107342396+blibilb@users.noreply.github.com>
2023-07-13 20:19:41 +0530

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,10 +12,9 @@ import aiohttp
 import sqlite3
 import os
 import openai,praw,asyncpraw
-reddit = asyncpraw.Reddit(client_id='3TTxM-vWr7BJAYmo4iIG8w',
-                     client_secret='tacBznSDMSAprY4ovYriDyU2-0CWnA',
+reddit = asyncpraw.Reddit(client_id='',
+                     client_secret='',
                      user_agent='china man')
-openai.api_key = "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"
 from io import StringIO, BytesIO
 from discord.ext import commands
 from asyncio import sleep
@@ -36,98 +35,10 @@ import requests
 import json
 import time
 import replicate
-os.environ["REPLICATE_API_TOKEN"] = 'cda13507234110706eed5d61378e1790df9819e0'
-replicate.Client(api_token=["REPLICATE_API_TOKEN"])
-model = replicate.models.get("stability-ai/stable-diffusion")
-version = model.versions.get("f178fa7a1ae43a9a9af01b833b9d2ecf97b1bcb0acfd2dc5dd04895e042863f1")
-
-# https://replicate.com/stability-ai/stable-diffusion/versions/f178fa7a1ae43a9a9af01b833b9d2ecf97b1bcb0acfd2dc5dd04895e042863f1#input
-
 message_list = {}
-BASE_URL = "https://api.luan.tools/api/tasks/"
-HEADERS = {
-    'Authorization': 'bearer oZFb5vfcICAbdga7mP05lg0vPogYHbGZ',
-    'Content-Type': 'application/json'
-}
 
 path=None
-def send_task_to_dream_api(prompt, style_id=3, target_img_path=None):
-    """
-    Send requests to the dream API.
-    prompt is the text prompt.
-    style_id is which style to use (a mapping of ids to names is in the docs).
-    target_img_path is an optional path to an image to influence the generation.
-    """
 
-    # Step 1) make a POST request to https://api.luan.tools/api/tasks/
-    post_payload = json.dumps({
-        path: bool(target_img_path)
-    })
-    post_response = requests.request(
-        "POST", BASE_URL, headers=HEADERS, data=post_payload)
-    
-    # Step 2) skip this step if you're not sending a target image otherwise,
-    # upload the target image to the url provided in the response from the previous POST request.
-    if target_img_path:
-        target_image_url = post_response.json()["target_image_url"]
-        with open(target_img_path, 'rb') as f:
-            fields = target_image_url["fields"]
-            fields ["file"] = f.read()
-            requests.request("POST", url=target_image_url["url"], files=fields)
-
-    # Step 3) make a PUT request to https://api.luan.tools/api/tasks/{task_id}
-    # where task id is provided in the response from the request in Step 1.
-    task_id = post_response.json()['id']
-    task_id_url = f"{BASE_URL}{task_id}"
-    put_payload = json.dumps({
-        "input_spec": {
-            "style": style_id,
-            "prompt": prompt,
-            "target_image_weight": 0.1,
-            "width": 960,
-            "height": 1560
-    }})
-    requests.request(
-        "PUT", task_id_url, headers=HEADERS, data=put_payload)
-
-    # Step 4) Keep polling for images until the generation completes
-    while True:
-        response_json = requests.request(
-            "GET", task_id_url, headers=HEADERS).json()
-
-        state = response_json["state"]
-
-        if state == "completed":
-            r = requests.request(
-                "GET", response_json["result"])
-            print(r)
-            with open("image.jpg", "wb") as image_file:
-                image_file.write(r.content)
-            return 'image.jpg'
-            print("image saved successfully :)")
-            break
-
-        elif state =="failed":
-            print("generation failed :(")
-            break
-
-        time.sleep(3)
-    
-    # Step 5) Enjoy your beautiful artwork :3
-
-
-m_offets = [
-    (-1, -1),
-    (0, -1),
-    (1, -1),
-    (-1, 0),
-    (1, 0),
-    (-1, 1),
-    (0, 1),
-    (1, 1)
-]
-
-m_numbers = [":one:", ":two:", ":three:", ":four:", ":five:", ":six:"]
 
 class Fun_Commands(commands.Cog):
     def __init__(self, bot):
